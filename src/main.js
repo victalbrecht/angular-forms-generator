57146967c613fd5d69be1ccf5c4f9bb8fb32693c
Victor Albrecht <victalbrecht@live.com>
2020-01-10 01:47:07 -0300

    base ui

--- src/main.js
+++ src/main.js
@@ -1,3 +1,3 @@
 import 'materialize-css/dist/js/materialize.min';
-import '@fortawesome/fontawesome-free/js/all'
-import './main.sass'
+import '@fortawesome/fontawesome-free/js/all';
+import './main.sass';
