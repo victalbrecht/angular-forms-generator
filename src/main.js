f0a12363ca8a5ab64f7bfa3582fc359b87a3ffff
Victor Albrecht <victalbrecht@live.com>
2020-01-11 00:33:51 -0300

    adjusting code-card sizes

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
+'use strict';
+
 import 'materialize-css/dist/js/materialize.min';
 import '@fortawesome/fontawesome-free/js/all';
 import './main.sass';
+
+M.Tabs.init(document.getElementById('codeTabs'));
