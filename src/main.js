7b63021ce6a4730e6843be0dc40467451a15e05b
Victor Albrecht <victalbrecht@live.com>
2020-01-15 03:22:31 -0300

    add/remove input card methods

--- src/main.js
+++ src/main.js
@@ -3,3 +3,25 @@
 import './bootstrap';
 import '@fortawesome/fontawesome-free/js/all';
 import './main.sass';
+import handlebars from 'handlebars/dist/handlebars';
+import * as mat from 'materialize-css/dist/js/materialize.min';
+
+let inputQuantity = 1;
+
+window.refreshBoilerplateData = () => {
+
+};
+
+window.deleteInputCard = inputId => {
+  const inputCard = document.querySelector(`#input-card-${inputId}`);
+  console.log(inputCard)
+  inputCard.parentNode.removeChild(inputCard);
+  inputQuantity--;
+};
+
+window.addInputCard = () => {
+  inputQuantity++;
+  const newInputCard = handlebars.compile(document.getElementById('input-card').innerHTML);
+  document.getElementById('inputs').innerHTML += newInputCard({ inputCardId: inputQuantity });  
+  mat.FormSelect.init(document.querySelector(`#input-card-${inputQuantity} select`));
+};
